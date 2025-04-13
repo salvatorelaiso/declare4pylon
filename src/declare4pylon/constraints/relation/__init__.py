98b4d03c81eaa3dd46ec2c2aee04bc7ede0f7c1a
salvatore <salvatorelaiso@gmail.com>
2025-04-13 20:54:41 +0200

    feat: add `CoExistenceConstraint` class and corresponding tests

--- src/declare4pylon/constraints/relation/__init__.py
+++ src/declare4pylon/constraints/relation/__init__.py
@@ -1,7 +1,9 @@
+from .co_existence import CoExistenceConstraint
 from .responded_existence import RespondedExistenceConstraint
 from .template import RelationTemplate
 
 __all__ = [
+    "CoExistenceConstraint",
     "RelationTemplate",
     "RespondedExistenceConstraint",
 ]

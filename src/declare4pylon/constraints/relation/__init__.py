b1aa2fd1a1836edae6eca59c9d75ce9f9a8d5108
salvatore <salvatorelaiso@gmail.com>
2025-04-14 00:25:47 +0200

    feat: add `ChainSuccessionConstraint` class and corresponding tests

--- src/declare4pylon/constraints/relation/__init__.py
+++ src/declare4pylon/constraints/relation/__init__.py
@@ -3,6 +3,7 @@ from .alternate_response import AlternateResponseConstraint
 from .alternate_succession import AlternateSuccessionConstraint
 from .chain_precedence import ChainPrecedenceConstraint
 from .chain_response import ChainResponseConstraint
+from .chain_succession import ChainSuccessionConstraint
 from .co_existence import CoExistenceConstraint
 from .precedence import PrecedenceConstraint
 from .responded_existence import RespondedExistenceConstraint
@@ -16,6 +17,7 @@ __all__ = [
     "AlternateSuccessionConstraint",
     "ChainPrecedenceConstraint",
     "ChainResponseConstraint",
+    "ChainSuccessionConstraint",
     "CoExistenceConstraint",
     "PrecedenceConstraint",
     "RelationTemplate",
---
+++ src/declare4pylon/constraints/relation/chain_succession.py
@@ -0,0 +1,16 @@
+import torch
+from jaxtyping import Bool, Int
+
+from declare4pylon.constraints.relation import ChainPrecedenceConstraint, ChainResponseConstraint
+
+from .template import RelationTemplate
+
+
+class ChainSuccessionConstraint(RelationTemplate):
+    """A and B occur in the process instance if and only if the latter immediately follows the former."""
+
+    def _condition(self, traces: Int[torch.Tensor, "batch activities"]) -> Bool[torch.Tensor, " batch"]:
+        return torch.logical_and(
+            ChainResponseConstraint(activity_a=self.activity_a, activity_b=self.activity_b)._condition(traces),  # noqa: SLF001
+            ChainPrecedenceConstraint(activity_a=self.activity_a, activity_b=self.activity_b)._condition(traces),  # noqa: SLF001
+        )

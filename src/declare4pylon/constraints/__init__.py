1e4c936b7e5e03efa0bd019016967944b893b555
Salvatore Laiso <32564922+salvatorelaiso@users.noreply.github.com>
2025-08-31 13:31:13 +0200

    refactor: reorganize imports (#8)
    * refactor: reorganize imports and update __all__ in constraints module
    
    * fix: ensure DeclareConstraint import is the first in the constraints module

--- src/declare4pylon/constraints/__init__.py
+++ src/declare4pylon/constraints/__init__.py
@@ -1,5 +1,44 @@
-from .constraint import DeclareConstraint
+# DeclareConstraint import must be the first import in this file
+from .constraint import DeclareConstraint  # noqa: I001
+
+from .choice import ChoiceConstraint, ExclusiveChoiceConstraint
+from .existence import (
+    AbsenceConstraint,
+    ExactlyConstraint,
+    ExistenceConstraint,
+    InitConstraint,
+)
+from .relation import (
+    AlternatePrecedenceConstraint,
+    AlternateResponseConstraint,
+    AlternateSuccessionConstraint,
+    ChainPrecedenceConstraint,
+    ChainResponseConstraint,
+    ChainSuccessionConstraint,
+    CoExistenceConstraint,
+    PrecedenceConstraint,
+    RespondedExistenceConstraint,
+    ResponseConstraint,
+    SuccessionConstraint,
+)
 
 __all__ = [
+    "AbsenceConstraint",
+    "AlternatePrecedenceConstraint",
+    "AlternateResponseConstraint",
+    "AlternateSuccessionConstraint",
+    "ChainPrecedenceConstraint",
+    "ChainResponseConstraint",
+    "ChainSuccessionConstraint",
+    "ChoiceConstraint",
+    "CoExistenceConstraint",
     "DeclareConstraint",
+    "ExactlyConstraint",
+    "ExclusiveChoiceConstraint",
+    "ExistenceConstraint",
+    "InitConstraint",
+    "PrecedenceConstraint",
+    "RespondedExistenceConstraint",
+    "ResponseConstraint",
+    "SuccessionConstraint",
 ]
